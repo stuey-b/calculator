8f9bec72185304eb6928d46430e456f0b32981a0
stuey-b <stuartwbartlett@gmail.com>
2023-03-16 22:16:15 +0900

    fix bugs and tidy code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const calculatorDisplay = document.getElementById(`display`);
+const subDisplay = document.getElementById(`sub-display`);
 const clearLastButton = document.getElementById(`clear-last`);
 const allClearButton = document.getElementById(`all-clear`);
 const decimalButton = document.getElementById(`decimal`);
@@ -7,121 +8,115 @@ const equalsButton = document.getElementById(`equals`);
 const numberButtons = document.querySelectorAll(`.number-btns`);
 const operatorButtons = document.querySelectorAll(`.operator-btns`);
 
-let displayNumbers = "";
+let storedNumbers = "";
+let temporaryNumbers = "";
 let chosenOperator = "";
-let temporaryResult = "";
 let calculatedResult = "";
 let hasDecimalPoint = false;
 
-const displayNumbersOnScreen = () => {
-  numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener(`click`, (e) => {
-      const clickedNumberKey = e.target.textContent;
-      //checks if there is a decimal point
-      if (clickedNumberKey === `.` && !hasDecimalPoint) {
-        hasDecimalPoint = true;
-        //terminate the function if a decimal exists
-      } else if (clickedNumberKey === `.` && hasDecimalPoint) {
-        return;
-      }
-      displayNumbers += clickedNumberKey;
-      limitInputLength();
-    });
-  });
-};
-displayNumbersOnScreen();
-
-operatorButtons.forEach((operatorButton) => {
-  operatorButton.addEventListener(`click`, (e) => {
-    
-    if (displayNumbers) {
-      hasDecimalPoint = false;
-      const clickedOperatorButton = e.target.textContent;
-    //check if we have 
-      if (displayNumbers && temporaryResult && chosenOperator) {
-      calculate();
-      } else {
-        calculatedResult = parseFloat(displayNumbers);
-      }
-      clearScreenForNextNumber(clickedOperatorButton);
-      chosenOperator = clickedOperatorButton;
-      console.log(calculatedResult);
-    } else {
-      // exit function if there are no numbers before the operator
-      return
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener(`click`, (e) => {
+    const clickedNumberKey = e.target.textContent;
+    if (clickedNumberKey === `.` && !hasDecimalPoint) {
+      hasDecimalPoint = true;
+    } else if (clickedNumberKey === `.` && hasDecimalPoint) {
+      return;
     }
-    // allow a decimal to be added for the next number
-    // hasDecimalPoint = false;
-    // const clickedOperatorButton = e.target.textContent;
-    // //check if we have 
-    //   if (displayNumbers && temporaryResult && chosenOperator) {
-    //   calculate();
-    //   } else {
-    //     calculatedResult = parseFloat(displayNumbers);
-    //   }
-    //   clearScreenForNextNumber(clickedOperatorButton);
-    //   chosenOperator = clickedOperatorButton;
-    //   console.log(calculatedResult);
-
+    storedNumbers += clickedNumberKey;
+    limitInputLength();
   });
 });
 
-const calculate = () => {
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener(`click`, (e) => {
+    //allow a decimal to be added if a number exists
+    if (storedNumbers) {
+      hasDecimalPoint = false;
+      const clickedOperatorButton = e.target.textContent;
+      if (storedNumbers && chosenOperator && temporaryNumbers) {
+        calculateAnswer();
+      } else {
+        calculatedResult = parseFloat(storedNumbers);
+      }
+      storeTempNumber(clickedOperatorButton);
+      chosenOperator = clickedOperatorButton;
+    } else {
+      return;
+    }
+  });
+});
+
+const calculateAnswer = () => {
   switch (chosenOperator) {
     case `+`:
-      calculatedResult = parseFloat(calculatedResult) + parseFloat(displayNumbers);
-      break
+      calculatedResult = parseFloat(calculatedResult) + parseFloat(storedNumbers);
+      break;
     case `-`:
-      calculatedResult = parseFloat(calculatedResult) - parseFloat(displayNumbers);
-      break
+      calculatedResult = parseFloat(calculatedResult) - parseFloat(storedNumbers);
+      break;
     case `x`:
-      calculatedResult = parseFloat(calculatedResult) * parseFloat(displayNumbers);
-      break
+      calculatedResult = parseFloat(calculatedResult) * parseFloat(storedNumbers);
+      break;
     case `÷`:
-      calculatedResult = parseFloat(calculatedResult) / parseFloat(displayNumbers);
-      break
+      calculatedResult = parseFloat(calculatedResult) / parseFloat(storedNumbers);
+      break;
   }
 };
+
+const storeTempNumber = (clickedOperatorButton) => {
+  temporaryNumbers += storedNumbers + "  " + clickedOperatorButton + " ";
+  subDisplay.textContent = temporaryNumbers;
+  calculatorDisplay.textContent = "";
+  storedNumbers = "";
+};
+
+equalsButton.addEventListener(`click`, () => {
+  if (storedNumbers) {
+    calculateAnswer();
+    storeTempNumber();
+    calculatorDisplay.textContent = calculatedResult;
+    subDisplay.textContent = "";
+    storedNumbers = calculatedResult;
+    temporaryNumbers = "";
+  } else {
+    return;
+  }
+});
 
 const clearLast = () => {
   clearLastButton.addEventListener(`click`, () => {
     calculatorDisplay.textContent = "0";
-    displayNumbers = "";
+    storedNumbers = "";
   });
 };
-clearLast();
 
 const clearAll = () => {
   allClearButton.addEventListener(`click`, () => {
     calculatorDisplay.textContent = "0";
-    displayNumbers = "";
-    temporaryResult = "";
+    subDisplay.textContent = "";
+    storedNumbers = "";
+    temporaryNumbers = "";
     calculatedResult = "";
+    console.clear();
   });
 };
-
-clearAll();
 
 const backspaceDelete = () => {
   backspaceButton.addEventListener(`click`, () => {
-    const clearedDisplay = (calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, -1));
-    displayNumbers = clearedDisplay;
+    const clearedDisplay = (calculatorDisplay.textContent =calculatorDisplay.textContent.slice(0, -1));
+    storedNumbers = clearedDisplay;
   });
 };
-backspaceDelete();
 
 const limitInputLength = () => {
-  if (displayNumbers.length >= 13) {
-    displayNumbers = displayNumbers.slice(0, 12);
+  if (storedNumbers.length >= 13) {
+    storedNumbers = storedNumbers.slice(0, 12);
   } else {
-    calculatorDisplay.textContent = displayNumbers;
+    calculatorDisplay.textContent = storedNumbers;
   }
 };
 
 
-
-function clearScreenForNextNumber() {
-  temporaryResult += displayNumbers
-  calculatorDisplay.textContent = "0";
-  displayNumbers = "";
-}
+clearAll();
+clearLast();
+backspaceDelete();
